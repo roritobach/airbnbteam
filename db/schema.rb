b14fbe611715602bd48dee8290bad51147d46d00
yan <perronyannig@hotmail.com>
2016-02-22 19:42:12 +0100

    ass user to announces, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222174752) do
+ActiveRecord::Schema.define(version: 20160222184002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160222174752) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "announces", ["user_id"], name: "index_announces_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 20160222174752) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "announces", "users"
 end
