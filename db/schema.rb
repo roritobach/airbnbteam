1b11171cc9cc534b89e4da1485031a0ac2db114f
bach georges <ferrenbachg@hotmail.fr>
2016-02-24 10:39:13 +0100

    model Product pour picture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222184002) do
+ActiveRecord::Schema.define(version: 20160224093826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20160222184002) do
   end
 
   add_index "announces", ["user_id"], name: "index_announces_on_user_id", using: :btree
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
